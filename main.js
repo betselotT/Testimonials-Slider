ffcc3462e99b4c116bff69d8d85dfc0ae57271a7
betsiT <betselottesfa9@gmail.com>
2023-11-08 07:24:48 -0900

    added button functionality

--- main.js
+++ main.js
@@ -14,15 +14,41 @@
     }
 
     function createCustomer(img, name, text){
+        // console.log(img)
         let Img = `./img/${img}.png`
-        let customer = new Customer(img, name, text)
+        let customer = new Customer(Img, name, text)
 
         customers.push(customer)
     }
 
     createCustomer(0, 'John', ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,itaque? Velit voluptatibus iure neque nesciunt voluptas pariaturtempore omnis consequuntur.')
-    createCustomer(1, 'John', ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,itaque? Velit voluptatibus iure neque nesciunt voluptas pariaturtempore omnis consequuntur.')
-    createCustomer(2, 'John', ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,itaque? Velit voluptatibus iure neque nesciunt voluptas pariaturtempore omnis consequuntur.')
-    createCustomer(3, 'John', ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,itaque? Velit voluptatibus iure neque nesciunt voluptas pariaturtempore omnis consequuntur.')
-    createCustomer(4, 'John', ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,itaque? Velit voluptatibus iure neque nesciunt voluptas pariaturtempore omnis consequuntur.')
+    createCustomer(1, 'Sandy', ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,itaque? Velit voluptatibus iure neque nesciunt voluptas pariaturtempore omnis consequuntur.')
+    createCustomer(2, 'Amy', ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,itaque? Velit voluptatibus iure neque nesciunt voluptas pariaturtempore omnis consequuntur.')
+    createCustomer(3, 'Tyrell', ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,itaque? Velit voluptatibus iure neque nesciunt voluptas pariaturtempore omnis consequuntur.')
+    createCustomer(4, 'Wanda', ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,itaque? Velit voluptatibus iure neque nesciunt voluptas pariaturtempore omnis consequuntur.')
+
+    btn.forEach(function(button) {
+        button.addEventListener('click', function(e) {
+            if(e.target.parentElement.classList.contains('prevBtn')) {
+                if(index === 0) {
+                    index = customers.length
+                }
+                index--
+                customerImage.src = customers[index].img
+                customerName.textContent = customers[index].name
+                customerText.textContent = customers[index].text
+            }
+
+            if(e.target.parentElement.classList.contains('nextBtn')) {
+                index++
+                if(index === customers.length) {
+                    index = 0
+                }
+                customerImage.src = customers[index].img
+                customerName.textContent = customers[index].name
+                customerText.textContent = customers[index].text
+            }
+        })
+    })
+    
 })()
